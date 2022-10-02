cd872dcaf636c9259b58a3786a704a42852d40c6
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-10-02 17:16:56 +0200

    No secure data is stored in environment files

---
+++ src/environments/environment_example.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'game-scoreboards',
+    appId: '',
+    storageBucket: 'game-scoreboards.appspot.com',
+    apiKey: '',
+    authDomain: 'game-scoreboards.firebaseapp.com',
+    messagingSenderId: '',
+  },
+  useEmulators: false,
+  production: true
+};
