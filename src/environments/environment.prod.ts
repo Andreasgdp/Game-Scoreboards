594db2a6fbb18a9c11f9b32a54129486d96c0be7
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-10-03 11:13:14 +0200

    Yes to environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
     projectId: 'game-scoreboards',
-    appId: '',
+    appId: '1:395707316276:web:cb934be84c17524313f261',
     storageBucket: 'game-scoreboards.appspot.com',
-    apiKey: '',
+    apiKey: 'AIzaSyDSfwX-_-jdPz1h0N-Ztep7iy3u86gTnQQ',
     authDomain: 'game-scoreboards.firebaseapp.com',
-    messagingSenderId: '',
+    messagingSenderId: '395707316276',
   },
   useEmulators: false,
   production: true
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   firebase: {
     projectId: 'demo-scoreboards',
-    appId: '',
+    appId: '1:395707316276:web:cb934be84c17524313f261',
     storageBucket: 'game-scoreboards.appspot.com',
-    apiKey: '',
+    apiKey: 'AIzaSyDSfwX-_-jdPz1h0N-Ztep7iy3u86gTnQQ',
     authDomain: 'game-scoreboards.firebaseapp.com',
-    messagingSenderId: '',
+    messagingSenderId: '395707316276',
   },
   useEmulators: true,
   production: false
