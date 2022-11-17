c5773d0e1779eb8221e6fe20de767b333f964096
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-11-18 00:10:40 +0100

    Init landing pages and sign in design

--- src/app/pages/Auth/sign-in/sign-in.component.ts
+++ src/app/pages/Auth/sign-in/sign-in.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '@services/Auth';
 import { MenubarService } from '@services/utils';
 import { MenuItem } from 'primeng/api';
@@ -8,22 +15,29 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent {
   items: MenuItem[];
   username = '';
   password = '';
+
+  userform: FormGroup = this.fb.group({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
   signInLoading = false;
   googleSignInLoading = false;
 
   constructor(
+    private fb: FormBuilder,
     public authService: AuthService,
     public menuBarService: MenubarService
   ) {
     this.items = menuBarService.getItems();
   }
-
-  ngOnInit(): void {}
 
   signIn() {
     this.signInLoading = true;
@@ -36,6 +50,17 @@ export class SignInComponent implements OnInit {
     this.googleSignInLoading = true;
     this.authService.GoogleAuth().then(() => {
       this.googleSignInLoading = false;
+    });
+  }
+
+  validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFormFields(control);
+      }
     });
   }
 }
